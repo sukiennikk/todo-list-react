24171843d907c1d2477b77bb0f287540ca0ad5c4
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-11-19 15:51:52 +0100

    section headerRow optimized

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,12 +3,15 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
    <section className="section">
+        <div className="section__headerRow">
             <h2 className="section__header">{title}</h2>
             {extraHeaderContent}
-            <div className="section__body">
-                {body}
-            </div>
-        </section>
+        </div>
+        <div className="section__body">
+        {body}
+        </div>
+    </section>
+
 );
 
 export default Section;
