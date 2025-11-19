15122f24c0c0e3208dd17d8c5797f918f0ce1f29
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-11-19 17:39:40 +0100

    toggleHideDone Function added

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = ({tasks, hideDone})=> (
+const Buttons = ({tasks, hideDone, toggleHideDone})=> (
     tasks.length > 0 && (
         <div className="task-list__buttons">
-            <button 
+            <button onClick={toggleHideDone}
                 className="task-list__button">
                 {hideDone ? "Pokaż" : "Ukryj"} ukończone
             </button>
