ca80ef3fab59ac6391cbeb3789c62121544da0e5
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-11-19 14:54:59 +0100

    minor mistake deleted

--- src/Container/index.js
+++ src/Container/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 const Container = ({children}) => (
-    <main ClassName="container">
+    <main className="container">
         {children}
     </main>
 );
