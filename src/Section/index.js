dfccc72fb02df32eb30be259a4e8d160771bdb61
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-11-19 14:47:50 +0100

    addidtional buttons deleted

--- src/Section/index.js
+++ src/Section/index.js
@@ -5,14 +5,6 @@ const Section = ({ title, body, extraHeaderContent }) => (
    <section className="section">
             <h2 className="section__header">{title}</h2>
             {extraHeaderContent}
-            <div>
-                <button className="task-list__button">
-                Ukryj ukończone
-                </button>
-                <button className="task-list__button" disabled>
-                Ukończ wszystkie
-                </button>
-                </div>
             <div className="section__body">
                 {body}
             </div>
