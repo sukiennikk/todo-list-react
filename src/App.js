494a1dd54dc0251773d0acd58522ebf67b598570
sukiennikk <sukiennikk@icloud.com>
2025-11-23 19:06:20 +0100

    local storage initialized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -7,10 +7,20 @@ import Header from "./Header";
 import Container from "./Container";
 
 function App() {
+
+const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [
+      { id: 1, content: "przejść na Reacta", done: false },
+      { id: 2, content: "zjeść obiad", done: true }
+    ];
+  });
+
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([ {id: 1, content: "przejść na Reacta", done: false},
-  {id: 2, content: "zjeść obiad", done: true}
-]);
+  
+   useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
   setHideDone(prevHideDone => !prevHideDone);
