da8b329acfbb96b97091962dca02a75122239c2a
sukiennikk <sukiennikk@icloud.com>
2025-11-25 19:47:49 +0100

    autofocus added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FormWrapper, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+
+        inputRef.current.focus();
     };
 
     return (
         <FormWrapper onSubmit={onFormSubmit}>
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
